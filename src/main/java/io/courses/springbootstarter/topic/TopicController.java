8a1fe4efb1dd1f32c99f8d8415f3ba382434cecf
Prajwal Bhat M R <prajwal.bhat.m.r@ericsson.com>
2020-01-31 16:00:18 +0530

    CRUD api's with standalone data is added

--- src/main/java/io/courses/springbootstarter/topic/TopicController.java
+++ src/main/java/io/courses/springbootstarter/topic/TopicController.java
@@ -1,10 +1,7 @@
 package io.courses.springbootstarter.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,8 +21,19 @@ public class TopicController {
         return topicService.getTopic(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/topics")
-    public void addTopic(){
-
+    @RequestMapping(method=RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
     }
+
+    @RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
+    }
+
 }
